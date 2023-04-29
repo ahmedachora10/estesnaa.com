0c7a7cced6f762bae9e1203422d733991aa54494
ahmedachora10 <gjr8195@gmail.com>
2023-04-29 18:24:23 +0100

    update

--- resources/views/front/services/services.blade.php
+++ resources/views/front/services/services.blade.php
@@ -8,11 +8,11 @@
                     <div class="col-lg-3 col-md-4 col-sm-6 col-12 d-flex align-items-start">
                         <div class="services-body bg-white w-100 my-2 rounded shadow">
                             <div class="services-img p-3 shadow-sm w-100"
-                                style="border:8px solid #fff ;height: 150px; background-image:url({{ asset($service->image) }}); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+                                style="border:10px solid #fff ;height: 150px; background-image:url({{ asset($service->image) }}); background-repeat: no-repeat; background-size: cover; background-position: center center;">
                                 {{-- <img src="{{ asset($service->image) }}" class="w-100" alt="thumb"> --}}
                             </div>
                             <div class="px-3 py-2">
-                                <h3 class="mb-2 fw-bold" style=" font-size: 16px;line-height: 1.3em;"><a
+                                <h3 class="mb-2 mt-1 fw-bold" style=" font-size: 16px;line-height: 1.3em;"><a
                                         href="{{ route('front.services.show', $service) }}">{{ $service->name }}</a>
                                 </h3>
                                 <h6 class="text-secondary my-2" style="font-size:14px !important"><i
