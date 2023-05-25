587b94df091584e9ace636a1ed1a26990bfd4b5b
ahmedachora10 <gjr8195@gmail.com>
2023-05-25 21:11:23 +0100

    update

--- resources/views/livewire/inventors-filter.blade.php
+++ resources/views/livewire/inventors-filter.blade.php
@@ -49,10 +49,10 @@
 
                     @foreach ($inventors as $inventor)
                         <div class="col-md-4 col-sm-4">
-                            <div class="card home">
+                            <div class="card home w-100">
                                 <div class="card person">
                                     <div class="card-body">
-                                        <div class="ratio ratio-16x9">
+                                        <div class="ratio ratio-4x3">
                                             <img src="{{ asset($inventor->avatar) }}" class="card-img-top"
                                                 alt="...">
                                         </div>
