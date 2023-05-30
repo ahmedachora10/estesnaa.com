05b6a8c0d5a7a31c4069e459aa8ff2572ee8ed4f
ahmedachora10 <gjr8195@gmail.com>
2023-05-30 14:00:53 +0100

    update send money notification blade

--- resources/views/components/notifications/send-money.blade.php
+++ resources/views/components/notifications/send-money.blade.php
@@ -1,19 +1,22 @@
 @if ($checkType)
-    <a href="{{ isset($notification->data['link']) ? $notification->data['link'] : '#!' }}" class="d-flex">
-        <div class="flex-shrink-0 me-3">
-            <div class="avatar">
-                <span class="avatar-initial rounded-circle bg-label-success"><i class="bx bx-wallet"></i></span>
+    <a href="{{ isset($notification->data['link']) ? $notification->data['link'] : '#!' }}">
+        <div class="d-flex">
+            <div class="flex-shrink-0 me-3">
+                <div class="avatar">
+                    <span class="avatar-initial rounded-circle bg-label-success"><i class="bx bx-wallet"></i></span>
+                </div>
             </div>
-        </div>
 
-        <div class="flex-grow-1">
-            <h6 class="mb-1">{!! $notification->data['title'] !!}</h6>
-            <p class="mb-0">{{ $notification->data['content'] }}</p>
-            <small class="text-muted">{{ $notification->created_at->diffForHumans() }}</small>
-        </div>
-        <div class="flex-shrink-0 dropdown-notifications-actions">
-            <a href="javascript:void(0)" class="dropdown-notifications-read"><span class="badge badge-dot"></span></a>
-            <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="bx bx-x"></span></a>
+            <div class="flex-grow-1">
+                <h6 class="mb-1">{!! $notification->data['title'] !!}</h6>
+                <p class="mb-0">{{ $notification->data['content'] }}</p>
+                <small class="text-muted">{{ $notification->created_at->diffForHumans() }}</small>
+            </div>
+            <div class="flex-shrink-0 dropdown-notifications-actions">
+                <a href="javascript:void(0)" class="dropdown-notifications-read"><span
+                        class="badge badge-dot"></span></a>
+                <a href="javascript:void(0)" class="dropdown-notifications-archive"><span class="bx bx-x"></span></a>
+            </div>
         </div>
     </a>
 @endif
