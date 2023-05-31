4e87091f9dd2a2a374b744a3a7afab00ae6fe257
ahmedachora10 <gjr8195@gmail.com>
2023-05-31 03:30:16 +0100

    add a new job

--- app/Jobs/RemoveTemporaryFiles.php
+++ app/Jobs/RemoveTemporaryFiles.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class RemoveTemporaryFiles
 {
@@ -9,6 +10,8 @@ class RemoveTemporaryFiles
     public function __invoke()
     {
         $temporaryFiles = File::files(storage_path('app/livewire-tmp'));
+
+        Storage::put(storage_path('app/tests.txt'), 'Cron job running!');
 
         foreach ($temporaryFiles as $file) {
             File::delete($file);
