c05ad2b76a2f81a222312fb33faa253928a7ce8e
ahmedachora10 <gjr8195@gmail.com>
2023-05-31 03:49:20 +0100

    add a new command

---
+++ app/Console/Commands/RemoveTemporaryFiles.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class RemoveTemporaryFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tmpfiles:remove';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove Temporary Files';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $temporaryFiles = File::files(storage_path('app/livewire-tmp'));
+
+        Storage::put(storage_path('app/tests.txt'), 'Cron job running!');
+
+        foreach ($temporaryFiles as $file) {
+            File::delete($file);
+        }
+        $this->info('Temporary files deleted');
+        return Command::SUCCESS;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(new SubscriptionExpired)->everyMinute();
         $schedule->call(new PendingBalanceDuration)->everyMinute();
-        $schedule->call(new RemoveTemporaryFiles)->dailyAt('5:45');
+        $schedule->command('tmpfiles:remove')->dailyAt('17:50');
     }
 
     /**
